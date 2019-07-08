4c6fbca9e9a50176e180641593991fa52fb3a369
Saami PERDRIX <saami.perdrix@cgi.com>
2019-07-08 16:04:38 +0200

    exercice 1.5: blogAction

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    private static $defaultLocale = "fr";	
+
      /**
      * @Route("/default", name="default")
      */
@@ -17,15 +19,30 @@ class DefaultController extends AbstractController
         ]);
     }
 
-   /**
-     * @Route("/default", name="default")
-     */
-    public function index()
+
+    /**
+    * @Route("/blog/{_locale}/{year}/{title}", name="blog", requirements = { "_locale" = "fr|en", "year" = "\d{4}", "title" = "[[:alnum:]-]+" } )
+    *
+    */
+    public function blogAction($_locale, $year, $title)
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+	    return $this->render("default/blog.$_locale.html.twig", [
+           	'_locale' => $_locale,
+		'year' 	  => $year,
+		'title'	  => str_replace($title,'-',' '),	
         ]);
     }
+
+  /**
+    * @Route("/blog/{year}/{title}", name="blogDefaultLocale", requirements = { "year" = "\d{4}", "title" = "[[:alnum:]-]+" } )
+    *
+    */
+    public function blogDefaultLocaleAction($year, $title)
+    {
+	    //return $this->blogAction($this::$defaultLocale, $year, $title);
+	    return $this->redirectToRoute('blog', [ '_locale' => $this::$defaultLocale, 'year' => $year, 'title' => $title ] );
+    }
+
 
 
 }
